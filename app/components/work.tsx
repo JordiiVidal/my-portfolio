d273b86d62f2572bb22b1c1bfe8fddcab087955e
Vidiic <jordividal1997@gmail.com>
2024-02-15 20:56:29 +0100

    fix styles

--- app/components/work.tsx
+++ app/components/work.tsx
@@ -52,10 +52,10 @@ export default function Work(){
                                 <h4 className="mb-0 text-xl font-semibold">{ work.company }</h4>
                             </div>
                             <div className="mb-6 ml-4">
-                                <span className="text-sm text-neutral-700 dark:text-neutral-500">
+                                <span className="text-sm text-neutral-500">
                                     { work.title}, {expiranceDates(work)}
                                 </span>
-                                <div className="mt-4 mb-6 text-sm text-neutral-600 dark:text-neutral-400 ">
+                                <div className="mt-4 mb-6 text-sm text-neutral-300 text-pretty">
                                    { work.description }
                                 </div> 
                                 <ChipsTecnologies idWorkedTecnologyList={work.tecnologies} /> 
--- app/ui/external-link.tsx
+++ app/ui/external-link.tsx
@@ -8,7 +8,7 @@ interface ExternalLinkProps {
 
 const ExternalLink: React.FC<ExternalLinkProps> = ({text, icon }) => {
     return (
-        <span className="not-prose border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded text-sm inline-flex items-center leading-4 text-neutral-900 dark:text-neutral-100 no-underline py-0.5 px-2 cursor-default">
+        <span className="not-prose border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded text-sm inline-flex items-center leading-4 text-neutral-900 dark:text-neutral-100 no-underline py-0.5 px-2">
             {icon && <Image
                 src={`/icons/${icon}`}
                 className="rounded-full mr-2"
