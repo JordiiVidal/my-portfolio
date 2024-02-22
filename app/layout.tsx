c5ce727606376b9964cfa4088de776fc5b9b78fe
Vidiic <jordividal1997@gmail.com>
2024-02-22 20:24:53 +0100

    prettier general changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,29 @@
 import type { Metadata } from "next";
-import { inter } from '@/app/fonts/inter';
-import '@/app/styles/global.css';
+
+import { inter } from "@/app/fonts/inter";
+import "@/app/styles/global.css";
 
 export const metadata: Metadata = {
   title: "Jordi Vidal",
-  description: "Hello, I'm Jordi Vidal Roselló. I Full-Stack Developer from Barcelon.",
+  description:
+    "Hello, I'm Jordi Vidal Roselló. I Full-Stack Developer from Barcelon.",
   applicationName: "Jordi Vidal Portfolio",
-  authors:[{name: "Jordi Vidal"}],
-  generator: 'Next.js',
-  keywords:['portfolio', 'development', 'opportunity', 'employee', 'angular', 'javascript', 'typescript', 'full stack development', 'front end development'],
-  creator: 'Jordi Vidal',
-  publisher: 'Jordi Vidal',
-  metadataBase: new URL('https://my-portfolio-vidiic.vercel.app/'),
+  authors: [{ name: "Jordi Vidal" }],
+  generator: "Next.js",
+  keywords: [
+    "portfolio",
+    "development",
+    "opportunity",
+    "employee",
+    "angular",
+    "javascript",
+    "typescript",
+    "full stack development",
+    "front end development",
+  ],
+  creator: "Jordi Vidal",
+  publisher: "Jordi Vidal",
+  metadataBase: new URL("https://my-portfolio-vidiic.vercel.app/"),
 };
 
 export default function RootLayout({
@@ -21,10 +33,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body 
-        suppressHydrationWarning={true}
-        className={inter.className}
-      >
+      <body suppressHydrationWarning={true} className={inter.className}>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
+import Footer from "@layout/footer";
 import Header from "@layout/header";
 import { About, Stack, Work } from "./pages";
-import Footer from "@layout/footer";
 
 export default function Home() {
   return (
