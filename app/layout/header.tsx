2fe8e8f034d0cfb86d83731e466a7f58a1aa22cf
Vidiic <jordividal1997@gmail.com>
2024-02-22 20:19:12 +0100

    prettier layout

--- app/layout/header.tsx
+++ app/layout/header.tsx
@@ -1,65 +1,68 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect } from "react";
 
 const navigations = [
-    { name: "Home" , link: "#about"},
-    { name: "Stack" , link: "#stack"},
-    { name: "Work" , link: "#work"},
-    { name: "Projects" , link: "#"},
+  { name: "Home", link: "#about" },
+  { name: "Stack", link: "#stack" },
+  { name: "Work", link: "#work" },
+  { name: "Projects", link: "#" },
 ];
-  
+
 export default function Header() {
+  const [isOpen, setOpen] = React.useState(false);
 
-    const [isOpen, setOpen] = React.useState(false);
+  useEffect(() => {
+    document.body.classList.toggle("overflow-hidden", isOpen);
 
-    useEffect(() => {
-        document.body.classList.toggle('overflow-hidden', isOpen);
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, [isOpen]);
 
-        return () => {
-            document.body.classList.remove('overflow-hidden');
-        };
-    }, [isOpen]);
-    
-    function handleClick(){
-        setOpen(!isOpen);
+  function handleClick() {
+    setOpen(!isOpen);
+  }
+
+  function handleLinkClick() {
+    if (isOpen) {
+      setOpen(false);
     }
+  }
 
-    function handleLinkClick(){
-        if(isOpen){
-            setOpen(false);
-        }
-    }
-    
-    return (
-        <header>
-            <div className="fixed inset-x-0 top-0 z-50 md:backdrop-blur backdrop-blur-2xl duration-200 border-b  bg-zinc-900/500  border-zinc-800 ">
-                <nav className="container grid md:grid-cols-3 grid-cols-2 p-4 mx-auto">
-                    <div className="font-semibold">Portfolio.</div>
-                    <button className="flex md:hidden justify-end" onClick={handleClick}>
-                        <Image 
-                            src={`/header/${isOpen ? 'close' : 'menu'}.svg`}
-                            alt="menu"
-                            width={20}
-                            height={20}
-                        />
-                    </button>
-                    <ul className={`${isOpen ? 'flex' : 'hidden'} duration-500 md:flex md:flex-row flex-col justify-center md:gap-6 gap-8 md:h-fit h-[100vh]`}>
-                        {navigations.map((item) => (
-                            <Link 
-                                key={item.name}
-                                href={item.link}
-                                onClick={handleLinkClick}
-                                className="md:text-sm text-2xl duration-200 text-zinc-400 hover:text-zinc-100" 
-                            >
-                                { item.name }
-                            </Link>
-                        ))}
-                    </ul>
-                </nav>
-            </div>
-        </header>
-    );
+  return (
+    <header>
+      <div className="fixed inset-x-0 top-0 z-50 md:backdrop-blur backdrop-blur-2xl duration-200 border-b  bg-zinc-900/500  border-zinc-800 ">
+        <nav className="container grid md:grid-cols-3 grid-cols-2 p-4 mx-auto">
+          <div className="font-semibold">Portfolio.</div>
+          <button className="flex md:hidden justify-end" onClick={handleClick}>
+            <Image
+              src={`/header/${isOpen ? "close" : "menu"}.svg`}
+              alt="menu"
+              width={20}
+              height={20}
+            />
+          </button>
+          <ul
+            className={`${
+              isOpen ? "flex" : "hidden"
+            } duration-500 md:flex md:flex-row flex-col justify-center md:gap-6 gap-8 md:h-fit h-[100vh]`}
+          >
+            {navigations.map((item) => (
+              <Link
+                key={item.name}
+                href={item.link}
+                onClick={handleLinkClick}
+                className="md:text-sm text-2xl duration-200 text-zinc-400 hover:text-zinc-100"
+              >
+                {item.name}
+              </Link>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
