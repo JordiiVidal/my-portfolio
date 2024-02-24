c55d51dbd28ca7d7791bbfa240da9c88436db024
Vidiic <jordividal1997@gmail.com>
2024-02-24 23:41:58 +0100

    border cards

--- app/ui/card.tsx
+++ app/ui/card.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 const Card: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="overflow-hidden relative duration-700 rounded-xl bg-zinc-800/10 group md:gap-8 border-neutral-700 hover:bg-neutral-800 p-1 text-xs text-neutral-900 hover:text-neutral-100 no-underline">
+    <div className="overflow-hidden relative duration-700 rounded-xl bg-zinc-800/10 group md:gap-8 border border-neutral-700 hover:bg-neutral-800 p-1 text-xs text-neutral-900 hover:text-neutral-100 no-underline">
       {children}
     </div>
   );
