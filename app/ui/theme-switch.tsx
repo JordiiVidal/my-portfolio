6912de40cf41f94991b135cb2f740dc6f205419e
Vidiic <jordividal1997@gmail.com>
2024-02-28 19:42:11 +0100

    unify className

--- app/ui/theme-switch.tsx
+++ app/ui/theme-switch.tsx
@@ -6,19 +6,16 @@ import { FiMoon, FiSun } from "react-icons/fi";
 export default function ThemeSwitch() {
   const { setTheme, resolvedTheme } = useTheme();
 
-  if (resolvedTheme === "light") {
-    return (
-      <FiMoon
-        className="text-base text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-200 cursor-pointer"
-        onClick={() => setTheme("dark")}
-      />
-    );
-  }
+  const handleToggleTheme = () => {
+    setTheme(resolvedTheme === "light" ? "dark" : "light");
+  };
 
   return (
-    <FiSun
+    <div
       className="text-base text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-200 cursor-pointer"
-      onClick={() => setTheme("light")}
-    />
+      onClick={handleToggleTheme}
+    >
+      {resolvedTheme === "light" ? <FiMoon /> : <FiSun />}
+    </div>
   );
 }
