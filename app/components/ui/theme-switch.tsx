3b5db429e2774bb1f30a58c69848143ae877d36e
Vidiic <jordividal1997@gmail.com>
2024-03-02 19:56:31 +0100

    custom hook & default icon

--- app/components/ui/theme-switch.tsx
+++ app/components/ui/theme-switch.tsx
@@ -3,36 +3,45 @@ import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 import { FiMoon, FiSun } from "react-icons/fi";
 
+const useDarkTheme = () => {
+  const { resolvedTheme, setTheme } = useTheme();
+  const [isDarkTheme, setIsDarkTheme] = useState(resolvedTheme === "dark");
+
+  const setDarkTheme = (value: boolean) => {
+    setIsDarkTheme(value);
+    setTheme(value ? "dark" : "light");
+  };
+
+  return { isDarkTheme, setDarkTheme };
+};
+
 export default function ThemeSwitch() {
   const [mounted, setMounted] = useState(false);
-  const { setTheme, resolvedTheme } = useTheme();
+  const { setDarkTheme, isDarkTheme } = useDarkTheme();
   const controls = useAnimation();
 
   const handleClick = () => {
-    setTheme(resolvedTheme === "dark" ? "light" : "dark");
+    setDarkTheme(!isDarkTheme);
   };
 
   useEffect(() => {
     setMounted(true);
     controls.start({
       scale: [0.79, 1],
-      rotate: resolvedTheme === "dark" ? [0, 90] : [90, 0],
+      rotate: isDarkTheme ? [0, 90] : [90, 0],
       transition: {
         ease: "easeInOut",
       },
     });
-  }, [resolvedTheme, controls]);
+  }, [isDarkTheme, controls]);
 
-  if (!mounted) {
-    return <div></div>;
-  }
   return (
     <motion.div
       className="text-base text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-200 cursor-pointer"
       onClick={handleClick}
       animate={controls}
     >
-      {resolvedTheme === "dark" ? <FiSun /> : <FiMoon />}
+      {mounted && isDarkTheme ? <FiSun /> : <FiMoon />}
     </motion.div>
   );
 }
